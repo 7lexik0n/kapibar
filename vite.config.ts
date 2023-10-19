7a038cb2094eb5b40709844b0b830ab690dd22d3
Aleksey Kadyrov <7lexik0n@gmail.com>
2023-10-19 23:22:27 +0300

    Added link for alpha-testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/kapibar/',
+  base: './',
   build: {
     target: 'es2015',
     cssMinify: true,
-  }
+  },
+  mode: 'production',
 });
